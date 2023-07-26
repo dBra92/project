42dfd8477fc109bde83091415d6a1ab59650373c
Dan92 <dan92br@gmail.com>
2023-07-26 15:42:18 -0300

    Payroll

--- Estudo/src/main/kotlin/Main.kt
+++ Estudo/src/main/kotlin/Main.kt
@@ -1,12 +1,40 @@
 fun main(args: Array<String>) {
     //classe Instancia Objeto
-    var person: Person = Person(1992, "Dan")
+    /*var person: Person = Person(1992, "Dan")
     person.name
     person.wakeup()
+    person.doc
+     */
+    //Initiation("Jorge")
+    var ok = Animal("German")
+    ok.talk = "Guten tag auu"
 }
 
-class Person(val birthdate: Int, val name:String){
-    fun wakeup(){
+class Person(val birthdate: Int, var name: String) {
 
+    var doc: String? = null
+    constructor(birthdate: Int, name: String, code: Int) : this(birthdate, name) {
+        this.doc = doc
     }
+    fun wakeup() {}
+}
+
+class Initiation(var name:String){
+    init { // Fazer verificações de valores
+        print("Ola ")
+    }
+    init {
+        println("mundo")
+    }
+}
+
+class Animal(var type:String){
+    var talk: String = ""
+        get(){
+            println("Acess get")
+            return field
+        }
+        set(value){
+            field = talk
+        }
 }
