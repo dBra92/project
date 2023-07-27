bfafc64767797cf81a713de603744c0a2eed1d6f
Dan92 <dan92br@gmail.com>
2023-07-27 16:02:32 -0300

    Ribbon

--- spring/hr-worker/src/main/java/com/prfs/microservices/resources/WorkerResources.java
+++ spring/hr-worker/src/main/java/com/prfs/microservices/resources/WorkerResources.java
@@ -2,7 +2,10 @@ package com.prfs.microservices.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +19,13 @@ import com.prfs.microservices.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResources { // API basica para rodar na web
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+
 	@Autowired
 	private WorkerRepository wRepository;
+
+	@Autowired
+	private Environment env;
 
 	// Encapsula uma resposta http
 	@GetMapping
@@ -26,8 +34,11 @@ public class WorkerResources { // API basica para rodar na web
 		return ResponseEntity.ok(wList);
 	}
 
-	@GetMapping(value = "/{id}")		 // Para reconhecer o id na requisição
+	@GetMapping(value = "/{id}") // Para reconhecer o id na requisição
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		// N º da porta que irá rodar
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+
 		Worker oWorker = wRepository.findById(id).get(); // findByID retorna um Optional
 		return ResponseEntity.ok(oWorker);
 	}
