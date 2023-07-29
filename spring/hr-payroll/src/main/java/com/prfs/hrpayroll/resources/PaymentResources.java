f50ed53d2ea58ee0ec5bc1e5ce158e4ecc6e48ca
Dan92 <dan92br@gmail.com>
2023-07-29 10:29:45 -0300

    Hystrix

--- spring/hr-payroll/src/main/java/com/prfs/hrpayroll/resources/PaymentResources.java
+++ spring/hr-payroll/src/main/java/com/prfs/hrpayroll/resources/PaymentResources.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.prfs.hrpayroll.entities.Payment;
 import com.prfs.hrpayroll.services.PaymentService;
 
@@ -17,9 +18,17 @@ public class PaymentResources { // End point
 	@Autowired
 	private PaymentService sPayment;
 
+	// Nome do metodo alternativo caso ocorra alguma falha na chamada do metodo ResponseEntity...
+
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative") // Alternativo para falhas
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = sPayment.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Brann", 400.0, days);
 		return ResponseEntity.ok(payment);
 	}
 }
