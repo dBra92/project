f5a7f550ed877cde7259eae955f10fe38b043b2f
Dan92 <dan92br@gmail.com>
2023-07-29 10:39:38 -0300

    Criaçao do projeto gateway

---
+++ spring/hr-api-gateway-zuul/src/test/java/com/prfs/hrgatewayapizuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prfs.hrgatewayapizuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
