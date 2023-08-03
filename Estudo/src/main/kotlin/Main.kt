8d1bae0c8f64a33b68a827547024857b76f30323
Dan92 <dan92br@gmail.com>
2023-08-03 13:25:04 -0300

    Teste com spring ms config

--- Estudo/src/main/kotlin/Main.kt
+++ Estudo/src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+import sun.jvm.hotspot.oops.CellTypeState.value
+import java.util.function.BinaryOperator
+import java.util.function.IntBinaryOperator
+
 fun main(args: Array<String>) {
     //classe Instancia Objeto
     /*var person: Person = Person(1992, "Dan")
@@ -6,35 +10,87 @@ fun main(args: Array<String>) {
     person.doc
      */
     //Initiation("Jorge")
-    var ok = Animal("German")
-    ok.talk = "Guten tag auu"
+    // var ok = Animal("German")
+    // ok.talk = "Guten tag auu"
+
+    val a: Int = 13
+    val b: Int = 31
+    for (c in IntArithmetics.values()) {
+        println("$c($a, $b) = ${c.apply(a, b)}")
+    }
+    println(Color.RED)
+    EnumClass.valueOf(value: String): EnumClass
+    EnumClass.values(): Array<EnumClass>
 }
 
 class Person(val birthdate: Int, var name: String) {
 
     var doc: String? = null
+
     constructor(birthdate: Int, name: String, code: Int) : this(birthdate, name) {
         this.doc = doc
     }
+
     fun wakeup() {}
 }
 
-class Initiation(var name:String){
+class Initiation(var name: String) {
     init { // Fazer verificações de valores
         print("Ola ")
     }
+
     init {
         println("mundo")
     }
 }
 
-class Animal(var type:String){
+class Animal(var type: String) {
     var talk: String = ""
-        get(){
+        get() {
             println("Acess get")
             return field
         }
-        set(value){
+        set(value) {
             field = talk
         }
+}
+
+// Enum
+enum class Direction {
+    NORTH, SOUTH, WEST, EAST
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+}
+
+// Classes anonimas
+/*If the enum class defines any members, separate the constant definitions from the member definitions with a semicolon.*/
+enum class ProtocolState {
+    WAITING {
+        override fun signal() = TALKING
+    },
+    TALKING {
+        override fun signal() = WAITING
+    };
+
+    abstract fun signal(): ProtocolState
+}
+
+/*An enum class can implement an interface (but it cannot derive from a class), providing either a common implementation of interface members for all the entries, or separate implementations for each entry within its anonymous class. This is done by adding the interfaces you want to implement to the enum class declaration as follows*/
+enum class IntArithmetics : BinaryOperator<Int>, IntBinaryOperator {
+    PLUS {
+        override fun apply(t: Int, u: Int): Int = t + u
+    },
+    TIMES {
+        override fun apply(t: Int, u: Int): Int = t * u
+    };
+
+    override fun applyAsInt(t: Int, u: Int) = apply(t, u)
+}
+
+enum class EnumClass(value: String){
+
 }
