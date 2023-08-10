bc4888f63ee7084d1ee6504eefd6cd8a7c7cfdfc
Dan92 <dan92br@gmail.com>
2023-08-10 17:08:52 -0300

    OAuth

---
+++ java_proj/vdffgf/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dan
+ *
+ */
+module vdffgf {
+}
---
+++ java_proj/vdffgf/src/vdffgf/comenale.java
@@ -0,0 +1,10 @@
+package vdffgf;
+
+public class comenale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("gfhgfj");
+	}
+
+}
