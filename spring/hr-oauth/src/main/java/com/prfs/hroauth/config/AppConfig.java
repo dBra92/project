f779c30adf45d009005ba8040a870cfb1f9304e0
Dan92 <dan92br@gmail.com>
2023-08-11 17:41:07 -0300

    JWT pt 1 e 2

--- spring/hr-oauth/src/main/java/com/prfs/hroauth/config/AppConfig.java
+++ spring/hr-oauth/src/main/java/com/prfs/hroauth/config/AppConfig.java
@@ -1,18 +1,37 @@
 package com.prfs.hroauth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@EnableWebSecurity
-public class AppConfig extends WebSecurityConfigurerAdapter {
+//extends WebSecurityConfigurerAdapter
+@Configuration
+public class AppConfig  {
+	// Aula 41 - Login e Tokek pt 1
+	@Bean
+	public BCryptPasswordEncoder bCPEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
+	@Bean
+	public JwtAccessTokenConverter jATConverter() {
+		JwtAccessTokenConverter tConverter = new JwtAccessTokenConverter();
+		tConverter.setSigningKey("MY-SECRET-KEY");
+		return tConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tStore() {
+		return new JwtTokenStore(jATConverter());
+	}
+/*
+	// Solução para aula 40 - UserFeignClient
 	@Override
-
 	public void configure(HttpSecurity http) throws Exception {
-
 	// @formatter:off
 
     http
@@ -32,5 +51,5 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
     // @formatter:on
 
 	}
-
+*/
 }
--- spring/hr-oauth/src/main/java/com/prfs/hroauth/services/UserService.java
+++ spring/hr-oauth/src/main/java/com/prfs/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.prfs.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.prfs.hroauth.entities.User;
 import com.prfs.hroauth.feignclient.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger log = LoggerFactory.getLogger(UserService.class);
 
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("2 - Email not found");
 		}
 		log.info("Email found" + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = uFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			log.error("1 - email not found:" + username);
+			throw new UsernameNotFoundException("2 - Email not found");
+		}
+		log.info("Email found:" + username);
 		return user;
 	}
 }
