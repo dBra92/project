7c1c9273abee56973c1ebf1f667ef979991c7314
Dan92 <dan92br@gmail.com>
2023-08-15 14:23:00 -0300

    Configuraçao de segurança no servidor de configuraçao

--- spring/hr-api-gateway-zuul/src/main/java/com/prfs/hrgatewayapizuul/config/AppConfig.java
+++ spring/hr-api-gateway-zuul/src/main/java/com/prfs/hrgatewayapizuul/config/AppConfig.java
@@ -1,17 +1,23 @@
 package com.prfs.hrgatewayapizuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope //Atualizar os valores das variaveis
 @Configuration
 public class AppConfig {
-
+	
+	@Value("${jwt.secret}")
+	private String jSecret;
+	
 	@Bean
 	JwtAccessTokenConverter jATConverter() {
 		JwtAccessTokenConverter tConverter = new JwtAccessTokenConverter();
-		tConverter.setSigningKey("MY-SECRET-KEY");
+		tConverter.setSigningKey(jSecret);
 		return tConverter;
 	}
 
--- spring/hr-oauth/src/main/java/com/prfs/hroauth/config/AppConfig.java
+++ spring/hr-oauth/src/main/java/com/prfs/hroauth/config/AppConfig.java
@@ -1,9 +1,8 @@
 package com.prfs.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -11,6 +10,10 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 //extends WebSecurityConfigurerAdapter
 @Configuration
 public class AppConfig  {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecretOAuth;
+	
 	// Aula 41 - Login e Tokek pt 1
 	@Bean
 	public BCryptPasswordEncoder bCPEncoder() {
@@ -20,7 +23,7 @@ public class AppConfig  {
 	@Bean
 	public JwtAccessTokenConverter jATConverter() {
 		JwtAccessTokenConverter tConverter = new JwtAccessTokenConverter();
-		tConverter.setSigningKey("MY-SECRET-KEY");
+		tConverter.setSigningKey(jwtSecretOAuth);
 		return tConverter;
 	}
 
