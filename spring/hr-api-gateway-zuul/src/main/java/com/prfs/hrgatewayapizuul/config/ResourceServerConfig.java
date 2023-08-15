df2a4ade1fc85e06396a561bfdac11221819023e
Dan92 <dan92br@gmail.com>
2023-08-15 17:55:09 -0300

    Configuraçao de CORS

--- spring/hr-api-gateway-zuul/src/main/java/com/prfs/hrgatewayapizuul/config/ResourceServerConfig.java
+++ spring/hr-api-gateway-zuul/src/main/java/com/prfs/hrgatewayapizuul/config/ResourceServerConfig.java
@@ -1,13 +1,22 @@
 package com.prfs.hrgatewayapizuul.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -23,7 +32,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	private static final String[] OPERATOR = { "/hr-worker/**" };
 
 	// Perfil de admin
-	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**","/actuator/**", "/hr-worker/actuator/**", "/hr-oauth/actuator/**" };
+	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/actuator/**",
+			"/hr-oauth/actuator/**" };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -39,7 +49,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 				.hasAnyRole("ADMIN").anyRequest().authenticated());
 
 		// Autorizando requisições GET
-		// Somente o que estiver no vetor OPERATOR poderá ter acesso e somente no metodo GET
+		// Somente o que estiver no vetor OPERATOR poderá ter acesso e somente no metodo
+		// GET
+		
+		http.cors().configurationSource(cC());
+	}
+
+	@Bean
+	public CorsConfigurationSource cC() { // cC corsConfiguration
+		CorsConfiguration cc1 = new CorsConfiguration();
+		cc1.setAllowedOrigins(Arrays.asList("*"));
+		cc1.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		cc1.setAllowCredentials(true);
+		cc1.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource uBCCSource = new UrlBasedCorsConfigurationSource();
+		uBCCSource.registerCorsConfiguration("/**", cc1);
+		return uBCCSource;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> cF() { //cF corsFilter
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(cC()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE); // Precendencia mais alta
+		return bean;
 	}
 
 }
